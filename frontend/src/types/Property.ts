b718e08428d78feb82538f1cf63a2c00e1929a00
Nebenmor <nebenmor.anthony@gmail.com>
2025-06-27 10:43:04 +0100

    implement Property types

---
+++ frontend/src/types/Property.ts
@@ -0,0 +1,12 @@
+export interface Property {
+  id: number;
+  title: string;
+  description: string;
+  price: string;
+  location: string;
+  bedrooms: number;
+  bathrooms: number;
+  area: number;
+  image_url: string;
+  created_at: string;
+}
